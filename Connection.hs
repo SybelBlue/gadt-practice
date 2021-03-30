879df4e8462039610043e38ff868c695d507226e
L Caraco <lbc.dna@gmail.com>
2021-03-30 16:37:11 -0500

    added type families

--- Connection.hs
+++ Connection.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE DataKinds, GADTs #-}
 module Connection where
 
@@ -25,3 +25,20 @@ instance OpenOrAuth 'Auth
 -- expressing across a class of connections
 close :: OpenOrAuth s => Connection s -> Connection 'Closed
 close _ = ConnClosed
+
+-- A function over types
+type family Join (a :: ConnState) (b :: ConnState) 
+                :: ConnState where
+    Join 'Closed _ = 'Closed
+    Join _ 'Closed = 'Closed
+    Join 'Auth 'Auth = 'Auth 
+    Join _ _ = 'Open
+
+-- notice, this resolves
+--   Connection Open -> Connection Auth -> Connection (Join Open Auth)
+-- to
+--   Connection Open -> Connection Auth -> Connection Open
+join :: Connection a -> Connection b -> Connection (Join a b)
+join ConnClosed _ = ConnClosed
+join (ConnAuth s a) (ConnAuth _ _) = ConnAuth s a
+join _ _ = undefined  -- etc
