1bf0d23d3bc1c353ce05e4f73dfced22d16c7c22
L Caraco <lbc.dna@gmail.com>
2021-06-15 11:28:46 -0700

    trying to get list to work

--- List.hs
+++ List.hs
@@ -1,3 +1,10 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE GADTs #-}
 module List where
 
+import GHC.Types (Nat)
 
+data List a (n :: Nat) where
+    Cons :: a -> List a x -> List a ()
+    Nil  ::                List a 0
